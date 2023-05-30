1dbac544cce7d93bedc8df2aa769578228d2f3ef
Mohammad-Ashikul-Islam <blackcap10210@gmail.com>
2023-05-30 10:01:26 +0600

    cp

--- Codeforces/Untitled1.cpp
+++ Codeforces/Untitled1.cpp
@@ -3,21 +3,9 @@ using namespace std;
 
 #define ll long long
 
-//ref, in, out
-
-void swp( int a,int b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
 
 int main()
 {
-    int a=5;
-    if(a){
-        int a=6;
-        cout << a << " " << :a << endl;
-    }
+
     return 0;
 }
